9cdc84876f42b4a47f78ac1e689d1612842ce787
JangJaeSung <lancer98@naver.com>
2024-01-10 17:47:44 +0900

    240110

--- java/자바 실습/first/src/method2/MethodEx3.java
+++ java/자바 실습/first/src/method2/MethodEx3.java
@@ -38,15 +38,13 @@ public class MethodEx3 {
     }
     public static int deposit(int balance, int amount) {
         balance += amount;
-        System.out.println(amount + "원을 입금하였습니다. 현재 잔액: " + balance +
-            "원");
+        System.out.println(amount + "원을 입금하였습니다. 현재 잔액: " + balance + "원");
         return balance;
     }
     public static int withdraw(int balance, int amount) {
         if (balance >= amount) {
             balance -= amount;
-            System.out.println(amount + "원을 출금하였습니다. 현재 잔액: " + balance +
-                "원");
+            System.out.println(amount + "원을 출금하였습니다. 현재 잔액: " + balance + "원");
         } else {
             System.out.println(amount + "원을 출금하려 했으나 잔액이 부족합니다.");
         }
